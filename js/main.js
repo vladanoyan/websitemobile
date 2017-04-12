e271dec7e08df6f7c8f54c82de85176bca9d25ca
Vlad <anoyanvlad@gmail.com>
2017-04-12 19:36:19 +0400

    finish

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ $('.a').click(function() {
 });
 /*SCROLL PAGE*/
 
+$(document).ready(function() {
+  //variable for the 'stroke-dashoffset' unit
+  var $dashOffset = $(".path").css("stroke-dashoffset");
+  //on a scroll event - execute function
+  $(window).scroll(function() {
+    //calculate how far down the page the user is 
+    var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 200);
+    //convert dashoffset pixel value to interger
+    var $newUnit = parseInt($dashOffset, 10);
+    //get the value to be subtracted from the 'stroke-dashoffset'
+    var $offsetUnit = $percentageComplete * ($newUnit / 100);
+    //set the new value of the dashoffset to create the drawing effect
+    $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);
+  });
+});
